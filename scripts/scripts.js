943c4eaba1ccd246f448a319634e3835f11f8845
Brienyll <Brienyll29@gmail.com>
2020-01-27 11:10:04 -0800

    added styles and comments

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,6 @@
+// initializes api key and google api client on page load
 function init() {
+  //set api key
   gapi.client.setApiKey('AIzaSyCAIjyqYAvgYw_bs8fvHl_Cy8E-JKmhIIA');
   return gapi.client
     .load('https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest')
@@ -11,11 +13,16 @@ function init() {
       }
     );
 }
-// Make sure the client is loaded and sign-in is complete before calling this method.
-function execute() {
+// runs when search button is clicked
+function search() {
+  //read search query value
   const search = document.querySelector('#search').value;
+  // remove existing html on .videos
+  document.querySelector('.videos').innerHTML = '';
+  // returns the result of query - via api
   return gapi.client.youtube.search
     .list({
+      // set parameters needed - in this case this is by default by relevance
       part: 'snippet',
       q: search,
       type: 'video',
@@ -23,11 +30,14 @@ function execute() {
     })
     .then(
       function(response) {
-        console.log('Response', response);
         const videos = response.result.items;
+        // loop through results
         videos.forEach(item => {
+          // call getVideos function to separate variables and build html
           const video = getVideos(item);
+          //add html to .videos class
           const list = document.querySelector('.videos');
+          // push html videos
           list.innerHTML += video;
         });
       },
@@ -35,22 +45,28 @@ function execute() {
         console.error('Execute error', err);
       }
     );
+  // function to build each html video
   function getVideos(item) {
     const videoId = item.id.videoId;
     const videoTitle = item.snippet.title;
     const videoDescription = item.snippet.description;
-    return (videos = `<div class='container'><li class='videos'>
-    <h4><iframe class='vid' src='http://www.youtube.com/embed/${videoId}'></iframe>${videoTitle}</h4>
-    <p>${videoDescription}</p>
+    return (videos = `<div class='container'><li class='video'>
+    <iframe class='vid' src='http://www.youtube.com/embed/${videoId}'></iframe></<iframe>
+    <h3 class='videoTitle'>${videoTitle}</h3>
+    <p class='description'>${videoDescription}</p>
     </li></div>`);
   }
 }
 
 function filterDate() {
+  //read search query value
   const search = document.querySelector('#search').value;
+  // remove existing html on .videos
   document.querySelector('.videos').innerHTML = '';
+  // returns the result of query - via api
   return gapi.client.youtube.search
     .list({
+      // set parameters needed - in this case this is by date order descending
       part: 'snippet',
       q: search,
       type: 'video',
@@ -59,11 +75,14 @@ function filterDate() {
     })
     .then(
       function(response) {
-        console.log('Response', response);
+        // loop through results
         const videos = response.result.items;
         videos.forEach(item => {
+          // call getVideos function to separate variables and build html
           const video = getVideos(item);
+          //add html to .videos class
           const list = document.querySelector('.videos');
+          // push html videos
           list.innerHTML += video;
         });
       },
@@ -71,22 +90,30 @@ function filterDate() {
         console.error('Execute error', err);
       }
     );
+  // function to build each html video
   function getVideos(item) {
     const videoId = item.id.videoId;
     const videoTitle = item.snippet.title;
     const videoDescription = item.snippet.description;
-    return (videos = `<div class='container'><li class='videos'>
-    <h4><iframe class='vid' src='http://www.youtube.com/embed/${videoId}'></iframe>${videoTitle}</h4>
-    <p>${videoDescription}</p>
+    // JSX to use change variables on html output
+    return (videos = `<div class='container'><li class='video'>
+    <iframe class='vid' src='http://www.youtube.com/embed/${videoId}'></iframe></<iframe>
+    <h3 class='videoTitle'>${videoTitle}</h3>
+    <p class='description'>${videoDescription}</p>
     </li></div>`);
   }
 }
-
+// funtion runs when filter Date asc is clicked
 function filterDateAscending() {
+  //read search query value
   const search = document.querySelector('#search').value;
+  // remove existing html on .videos
   document.querySelector('.videos').innerHTML = '';
+  // returns the result of query - via api
   return gapi.client.youtube.search
     .list({
+      // set parameters needed - in this case this is by date order descending - but
+      // will be reversed later to make it ascending - can't do reverse via api request
       part: 'snippet',
       q: search,
       type: 'video',
@@ -95,12 +122,15 @@ function filterDateAscending() {
     })
     .then(
       function(response) {
-        console.log('Response', response);
         const videos = response.result.items;
+        // reverse the order results
         videos.reverse();
+        // call getVideos function to separate variables and build html
         videos.forEach(item => {
           const video = getVideos(item);
+          //add html to .videos class
           const list = document.querySelector('.videos');
+          // push html videos
           list.innerHTML += video;
         });
       },
@@ -108,22 +138,29 @@ function filterDateAscending() {
         console.error('Execute error', err);
       }
     );
+  // function to build each html video
   function getVideos(item) {
     const videoId = item.id.videoId;
     const videoTitle = item.snippet.title;
     const videoDescription = item.snippet.description;
-    return (videos = `<div class='container'><li class='videos'>
-    <h4><iframe class='vid' src='http://www.youtube.com/embed/${videoId}'></iframe>${videoTitle}</h4>
-    <p>${videoDescription}</p>
+    // JSX to use change variables on html output
+    return (videos = `<div class='container'><li class='video'>
+    <iframe class='vid' src='http://www.youtube.com/embed/${videoId}'></iframe></<iframe>
+    <h3 class='videoTitle'>${videoTitle}</h3>
+    <p class='description'>${videoDescription}</p>
     </li></div>`);
   }
 }
-
+//runs when filter view count desc is clicked
 function filterViewCount() {
+  //read search query value
   const search = document.querySelector('#search').value;
+  // remove existing html on .videos
   document.querySelector('.videos').innerHTML = '';
+  // returns the result of query - via api
   return gapi.client.youtube.search
     .list({
+      // set parameters needed - in this case this is by date order by view count
       part: 'snippet',
       q: search,
       type: 'video',
@@ -132,11 +169,13 @@ function filterViewCount() {
     })
     .then(
       function(response) {
-        console.log('Response', response);
         const videos = response.result.items;
         videos.forEach(item => {
+          // call getVideos function to separate variables and build html
           const video = getVideos(item);
+          //add html to .videos class
           const list = document.querySelector('.videos');
+          // push html videos
           list.innerHTML += video;
         });
       },
@@ -144,22 +183,30 @@ function filterViewCount() {
         console.error('Execute error', err);
       }
     );
+  // function to build each html video
   function getVideos(item) {
     const videoId = item.id.videoId;
     const videoTitle = item.snippet.title;
     const videoDescription = item.snippet.description;
-    return (videos = `<div class='container'><li class='videos'>
-    <h4><iframe class='vid' src='http://www.youtube.com/embed/${videoId}'></iframe>${videoTitle}</h4>
-    <p>${videoDescription}</p>
+    // JSX to use change variables on html output
+    return (videos = `<div class='container'><li class='video'>
+    <iframe class='vid' src='http://www.youtube.com/embed/${videoId}'></iframe></<iframe>
+    <h3 class='videoTitle'>${videoTitle}</h3>
+    <p class='description'>${videoDescription}</p>
     </li></div>`);
   }
 }
-
+//runs when filter view count asc is clicked
 function filterViewCountAscending() {
+  //read search query value
   const search = document.querySelector('#search').value;
+  // remove existing html on .videos
   document.querySelector('.videos').innerHTML = '';
+  // returns the result of query - via api
   return gapi.client.youtube.search
     .list({
+      // set parameters needed - in this case this is by date order by view count -
+      // will reverse the order later client-side
       part: 'snippet',
       q: search,
       type: 'video',
@@ -168,12 +215,15 @@ function filterViewCountAscending() {
     })
     .then(
       function(response) {
-        console.log('Response', response);
         const videos = response.result.items;
+        // reverse the order results
         videos.reverse();
         videos.forEach(item => {
+          // call getVideos function to separate variables and build html
           const video = getVideos(item);
+          //add html to .videos class
           const list = document.querySelector('.videos');
+          // push html videos
           list.innerHTML += video;
         });
       },
@@ -181,13 +231,16 @@ function filterViewCountAscending() {
         console.error('Execute error', err);
       }
     );
+  // function to build each html video
   function getVideos(item) {
     const videoId = item.id.videoId;
     const videoTitle = item.snippet.title;
     const videoDescription = item.snippet.description;
-    return (videos = `<div class='container'><li class='videos'>
-    <h4><iframe class='vid' src='http://www.youtube.com/embed/${videoId}'></iframe>${videoTitle}</h4>
-    <p>${videoDescription}</p>
+    // JSX to use change variables on html output
+    return (videos = `<div class='container'><li class='video'>
+    <iframe class='vid' src='http://www.youtube.com/embed/${videoId}'></iframe></<iframe>
+    <h3 class='videoTitle'>${videoTitle}</h3>
+    <p class='description'>${videoDescription}</p>
     </li></div>`);
   }
 }
